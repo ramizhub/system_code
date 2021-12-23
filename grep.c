b49db305ff8cd6cf3b192efd686fe7c459e8c3fd
ramiz <rearmttt@mail.ru>
2021-12-23 17:28:22 +0300

    Adding loops, debug and new comments

--- grep.c
+++ grep.c
@@ -1,59 +1,88 @@
-#include <stdio.h>
-#include <string.h>                    // strlen
-#include <unistd.h>
+    #include <stdio.h>                                      
+    #include <string.h>                                     // strlen()
+    #include <unistd.h>                                     // read()
 
-int main(int argc, char *argv[])       // two main function arguments
+
+int main(int argc, char *argv[])                            // 3    ./a.out users_word file_txt 
 {
-   if(argc != 3)                       // argument count must be 3 because first is function name, second is searched word, third is file's name
-   {  
-     printf("1Usage: %s string filename", argv[0]);        // if there is less or more than 3 arguments i will show my usage conditions
-     return 1;                                             // return looks like break in loops
-   }
+    
+    if(argc != 3)                                           // argCount should be three. the condition is not met - output usage
+    {  
+      printf("1Usage: %s string filename", argv[0]);        
+      return 1;                                             // termination
+    }
 
-   FILE *fpointer = fopen(argv[2], "r");                   // open file and creatin stdin
-   
-   
-   if(fpointer == NULL)                                    
-   {
-     perror("Could not open this file! Reason:  ");        // perror looks for the last error and shows it's number after my printed string
-     return 1;
-   }
+    
+    FILE *fpointer = fopen(argv[2], "r");                   // get file pointer
    
     
-   
-   int n_word_size = strlen( argv[1] );                    // that is word's length
-   int n_same_bytes = 0;                                   // same bytes count and that is zero because i need this zero at the begining
-   int n_bytes_read;                                       // i need to know for until which number loop should work
-   int file_descriptor = fileno(fpointer);                 // read's argument file descriptor
-   
-   char buffer[n_word_size];                               // i read from txt file as many bytes as characters in word
+    if(fpointer == NULL)                                    // null pointer returned if the program can't open file
+    {
+      perror("Could not open this file! Reason:  ");        // perror produces a message and describes the reasons
+      return 1;                                             // termination
+    }
 
-   while( (n_bytes_read = read(file_descriptor, buffer, n_word_size)) != 0 )       // if read returned zero this is file's end
-   {
-       for(int index = 0; index < n_bytes_read; index++)   // the second sentence is so important i need to read exactly as much as readed read function (n_bytes_read - 1)
-       {
-         if(  buffer[index] == argv[1][n_same_bytes]  )    // comparing the characters of the buffer and a two-dimensional array of the user     
-           n_same_bytes++;                                 // this one help me to know which element in argv[1] is already checked and which i need to try in the next loop
-                
-         else if(n_same_bytes == n_word_size )             // means that the number of characters you want is actually present
-         {  
-           printf("%s\n", argv[1]);                        // print user's word
-           n_same_bytes = 0;                               // after finding word i think that this one is right 
-         }
- 
-         else if(buffer[index] != argv[1][n_same_bytes])   // if at least one character in row is not that i need i need to refresh this because word is specific row
-           n_same_bytes = 0;
+      
+    int n_word_size = strlen( argv[1] );                    // size of uzer's word
+    int n_same_bytes = 0;                                   // same_bytes variable is 0 zero in the first iteration and when we find the wrong symbol
+    int n_bytes_read;                                       // n_bytes_read works with read() and takes the number of bytes read in each while iteration
+    int file_descriptor = fileno(fpointer);                 // first argument of read()
+    
+    
+    char buffer[n_word_size];                               // size of buffer equals size of word because in normal while iteration we read n_word_size bytes
+    char our_character;                                     // this variable is needed to memorize one single desired character and avoid reprocessing
+
+    
+    while( (n_bytes_read = read(file_descriptor, buffer, n_word_size)) != 0 )   // usually reads n_word_size bytes in buffer. max.size of buffer is n_word_size too
+    {
+      
+      
+      for(int index = 0; index < n_bytes_read; index++)     // for loop. every iteration here checks every byte of buffer. 
+      {   
+        
+        if(n_same_bytes == n_word_size)                     // if the character is not the last in the checking of for_loop and the word is found, it will appear here
+        {    
+          printf("%5s\n", argv[1]);                         // print this word with new line character.
+          our_character = '`';                              // give the character variable an unnecessary value
+          n_same_bytes = 0;                                 // looking for a new word again thanks to this variable
+        }
+
+        
+        if(buffer[index] == our_character)                  // character is needed character's copy and we must to skip it's checking
+          continue;
+        
+            
+        else if( buffer[index] == argv[1][n_same_bytes] )   // if current character is necessary character and it is not duplicate do these operations
+        {  
+          n_same_bytes++;                                   // this will make program check next character in argv[1][  ]  
+          our_character = buffer[index];                    // give special character current
+        }
+           
+           
+        else if( buffer[index] != argv[1][n_same_bytes] )   // if we read unnecessary character we skip it and make program check argv[1][  ] from begining
+        {
+          n_same_bytes = 0;                                     
+          our_character = '`';
+        }
+      
+      }
+      
+      
+      if(n_same_bytes == n_word_size)                       // if the character is the last in the checking of for_loop and the word is found, it will appear here
+      {    
+        printf("%5s\n", argv[1]);                           // print this word with new line character 
+        our_character = '`';                                // give the character variable an unnecessary value
+        n_same_bytes = 0;                                   // looking for a new word again thanks to this variable
+      } 
+    
+    }
 
 
-
-       }
-   }
-   
-   if(fclose(fpointer) != 0)                               // closing user's file and if 0 not returned error is here and perror will show it's number         
-   { 
-     perror("Could not close this one! Reason: ");        
-     return 1;
-   }
+    if(fclose(fpointer) != 0)                               // if 0 returned it tells about error in closing                    
+    { 
+      perror("Could not close this one! Reason: ");         // // perror produces a message and describes the reasons
+      return 1;
+    }
 
 
 }
