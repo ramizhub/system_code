c597238e1e78ab4325eea80622f9c5d8b5d7eeff
ramiz <rearmttt@mail.ru>
2021-12-24 01:49:03 +0300

    Adding _Bool and cut out the incorrect condition

--- grep.c
+++ grep.c
@@ -1,6 +1,7 @@
     #include <stdio.h>                                      
     #include <string.h>                                     // strlen()
     #include <unistd.h>                                     // read()
+    #include <stdbool.h>                                  
 
 
 int main(int argc, char *argv[])                            // 3    ./a.out users_word file_txt 
@@ -32,6 +33,8 @@ int main(int argc, char *argv[])                            // 3    ./a.out user
     char buffer[n_word_size];                               // size of buffer equals size of word because in normal while iteration we read n_word_size bytes
     char our_character;                                     // this variable is needed to memorize one single desired character and avoid reprocessing
 
+
+    _Bool word_is_near = false;
     
     while( (n_bytes_read = 
     read(file_descriptor, buffer, n_word_size)) != 0 )      // usually reads n_word_size bytes in buffer. max.size of buffer is n_word_size too
@@ -41,38 +44,39 @@ int main(int argc, char *argv[])                            // 3    ./a.out user
       for(int index = 0; index < n_bytes_read; index++)     // for loop. every iteration here checks every byte of buffer. 
       {   
         
-        if(n_same_bytes == n_word_size)                     // if the character is not the last in the checking of for_loop and the word is found, it will appear here
+        if(n_same_bytes == n_word_size && word_is_near)                     // if the character is not the last in the checking of for_loop and the word is found, it will appear here
         {    
           printf("%5s\n", argv[1]);                         // print this word with new line character.
-          our_character = '`';                              // give the character variable an unnecessary value
+          word_is_near = false;                             // user's word is far from this character
           n_same_bytes = 0;                                 // looking for a new word again thanks to this variable
         }
 
         
-        else if(buffer[index] == our_character)             // character is needed character's copy and we must to skip it's checking
+        else if(buffer[index] == our_character  && word_is_near )             // character is needed character's copy and we must to skip it's checking
           continue;
         
             
         else if( buffer[index] == argv[1][n_same_bytes] )   // if current character is necessary character and it is not duplicate do these operations
         {  
           n_same_bytes++;                                   // this will make program check next character in argv[1][  ]  
-          our_character = buffer[index];                    // give special character current
+          our_character = buffer[index];  
+          word_is_near = true;                              // we are near user's word
         }
            
            
         else if( buffer[index] != argv[1][n_same_bytes] )   // if we read unnecessary character we skip it and make program check argv[1][  ] from begining
         {
           n_same_bytes = 0;                                     
-          our_character = '`';
+          word_is_near = false;                             // user's word is far from this character
         }
       
       }
       
       
-      if(n_same_bytes == n_word_size)                       // if the character is the last in the checking of for_loop and the word is found, it will appear here
+      if(n_same_bytes == n_word_size && word_is_near)       // if the character is the last in the checking of for_loop and the word is found, it will appear here
       {    
         printf("%5s\n", argv[1]);                           // print this word with new line character 
-        our_character = '`';                                // give the character variable an unnecessary value
+        word_is_near = false;                               // user's word is far from this character
         n_same_bytes = 0;                                   // looking for a new word again thanks to this variable
       } 
     
