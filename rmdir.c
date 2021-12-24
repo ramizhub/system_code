3f36ce1537d50a66c1def7f32568827b9d47608c
ted mosby <tofig@freebsd.az>
2021-12-24 12:34:05 -0800

    Adding specification for rmdir utility

---
+++ rmdir.c
@@ -0,0 +1,4 @@
+/*
+    rmdir command receives 1 argument, which is the EMPTY directory to remove.
+    It exits 0 on success, and >0 if an error occurs. It does not print anything if the operation is successful.
+ */
