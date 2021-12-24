ed21d633253f5f5720c76626501141787f545b84
ramiz <rearmttt@mail.ru>
2021-12-25 00:06:14 +0300

    First commit here

--- mkdir.c
+++ mkdir.c
@@ -1,4 +1,27 @@
 /*
     mkdir command receives 1 argument, which is the new directory to create.
-    It exits 0 on success, and >0 if an error occurs. It does not print anything if the operation is successful.
+    It exits 0 on success, and >0 if an error occurs. It does not print anything if the operation is successful.
  */
+
+    #include <sys/stat.h>
+    #include <sys/types.h>
+    #include <stdio.h> 
+
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2)                                                               // this_function_name users_directory_name
+    {
+        printf("Usage -> %s directory_name\n");                                 // there u show user right usage 
+        return 1;                                                               
+    }
+
+    if(mkdir(argv[1]) != 0 )
+    {
+        perror("Error: ");
+        return -1;
+    }
+    
+    return 0;
+
+}
