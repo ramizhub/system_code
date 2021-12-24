d03b11399fca3f464f1f83716b45e645004eb93a
ramiz <rearmttt@mail.ru>
2021-12-25 00:08:02 +0300

    Fixing usage string

--- mkdir.c
+++ mkdir.c
@@ -12,11 +12,11 @@ int main(int argc, char *argv[])
 {
     if(argc != 2)                                                               // this_function_name users_directory_name
     {
-        printf("Usage -> %s directory_name\n");                                 // there u show user right usage 
+        printf("Usage -> %s directory_name\n", argv[0]);                                 // there u show user right usage 
         return 1;                                                               
     }
 
-    if(mkdir(argv[1]) != 0 )
+    if(mkdir( argv[1] ) != 0 )
     {
         perror("Error: ");
         return -1;
