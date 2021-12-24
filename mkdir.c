4522bc250b448cfde04f522b022901c26387e3ea
ted mosby <tofig@freebsd.az>
2021-12-24 12:23:08 -0800

    Adding specification for mkdir utility

---
+++ mkdir.c
@@ -0,0 +1,4 @@
+/*
+    mkdir command receives 1 argument, which is the new directory to create.
+    It exits 0 on success, and >0 if an error occurs. It does not print anything if the operation is successful.
+ */
