f5ebc88499241919b00d1165250d63a46e77c322
ramiz <rearmttt@mail.ru>
2021-12-25 17:31:32 +0300

    some comments changes

--- mkdir.c
+++ mkdir.c
@@ -8,15 +8,15 @@
     #include <stdio.h> 
 
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])                                                // main function arguments
 {
     if(argc != 2)                                                               // this_function_name users_directory_name
     {
-        printf("Usage -> %s directory_name\n", argv[0]);                        // there u show user right usage 
+        printf("Usage -> %s directory_name\n", argv[0]);                        // there i show user right usage 
         return 1;                                                               
     }
     
-    if(mkdir( argv[1], 740 ) != 0 )               // directory name and creation mode 
+    if(mkdir( argv[1], 740 ) != 0 )               // users_directory_name and creation_mode 
     {
         perror("Error: ");                        
         return -1;                                // mkdir standard bad return 
