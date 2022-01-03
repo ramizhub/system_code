034c774ad9839c1de6fdbddfb004d5e480c6e224
suletm <tofig@freebsd.az>
2022-01-03 08:13:47 -0800

    Update mkdir.c

--- mkdir.c
+++ mkdir.c
@@ -1,5 +1,7 @@
 /*
-    mkdir command receives 1 argument, which is the new directory to create.
+    mkdir command receives 2 arguments:
+           1. New directory to create.
+           2. Mode of the directory in octal (similar to chmod)
     It exits 0 on success, and >0 if an error occurs. It does not print anything if the operation is successful.
 */
 
