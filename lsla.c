b6d791b839a3cc9d8a08cabbe02b7085fb04919a
ramiz <rearmttt@mail.ru>
2022-01-06 22:17:56 +0300

    Clean up

--- lsla.c
+++ lsla.c
@@ -1,235 +1,167 @@
-    #include <stdio.h>                                                          // printf function
-    #include <string.h>                                                         // strlen function
+    #include <stdio.h>                                                          
+    #include <string.h>                                                         
     
-    #include <time.h>                                                           // ctime function
+    #include <time.h>                                                                     // ctime function
     
-    #include <dirent.h>                                                         // working with directory
-    #include <sys/types.h>                                                      // data types
-    #include <sys/stat.h>                                                       // data returned by the stat() function
-    #include <pwd.h>                                                            // password structure
+    #include <dirent.h>                                                         
+    #include <sys/types.h>                                                      
+    #include <sys/stat.h>                                                                 // for working with system data                                  
+    #include <pwd.h>                                                            
     #include <grp.h>
 
-int main(int argc, char * argv[])                                               // argc = 2, compiled_program directory_name
+int main(int argc, char * argv[])                                                         // 2 arguments
 {
     if(argc != 2)
     {
-        puts("Usage == >  compiled_program directory_name");                    // show conditions of usage
+        printf("usage: compiled_program file  or  compiled_program directory");
         return 1;
     }
-      
-    struct stat object_statistic;                                               // structure, which contains data about every file ( would contain )   
-    if( stat(argv[1], &object_statistic) != 0 )                                 // trying to find bugs with stat function ( segmentation fauled or file doesnt exist)
+ 
+    struct stat object_stat;                                                              // general structure ( for both of cases )
+    if( stat(argv[1], &object_stat) != 0)
     {
-        perror("Error! Reason == >  ");
+        perror("ls: stat error: ");
         return 1;
     } 
-
-
-    if((object_statistic.st_mode & S_IFMT) == S_IFREG)                          // checking argument with 1 index for being a regular file
+    
+    if( (object_stat.st_mode & S_IFMT) == S_IFREG )                                       // working with single file 
     {
-        struct passwd *user_id_statistic;                                       // passw structure. contains password and user's real name
-        struct group  *group_id_statistic;                                      // grouo structrue. contains real name of group, i need it
+        struct passwd * pass_stat;
+        struct group * group_stat;
         
         char * string_pointer;
         
-        size_t helpful_size;
         
-        stat(argv[1], &object_statistic); 
+        stat(argv[1], &object_stat);
+        pass_stat = getpwuid(object_stat.st_uid);                                         // pullin information for structures
+        group_stat = getgrgid(object_stat.st_gid);
+
+        string_pointer = ctime( &object_stat.st_mtime);                                   
+        string_pointer[strlen(string_pointer) - 1] = '\0';                                // executing new line character from returned string
+
+        putchar('-');                                                                     // anyway that is file
         
         
-        string_pointer = ctime( &object_statistic.st_mtime);                    
-        helpful_size = strlen(string_pointer);                                  // replacing new line character by null character
-        string_pointer[helpful_size - 1] = '\0'; 
-        putchar('-');
-        if(object_statistic.st_mode & S_IRUSR)
-            putchar('r');
-        else
-            putchar('-');
-           
-        if(object_statistic.st_mode & S_IWUSR)
-            putchar('w');
-        else
-            putchar('-');
-
-        if(object_statistic.st_mode & S_IXUSR)
-            putchar('x');
-        else
-            putchar('-');
-
-        if(object_statistic.st_mode & S_IRGRP)
-            putchar('r');
-        else
-            putchar('-');
-
-        if(object_statistic.st_mode & S_IWGRP)
-            putchar('w');
-        else
-            putchar('-');                                                       // asking st_mode: Can this read? Or write? Or execute?
-   
-        if(object_statistic.st_mode & S_IXGRP)
-            putchar('x');
-        else
-            putchar('-');
-
-        if(object_statistic.st_mode & S_IROTH)
-            putchar('r');
-        else
-            putchar('-');    
-
-        if(object_statistic.st_mode & S_IWOTH)
-            putchar('w');
-        else
-            putchar('-');     
-
-        if(object_statistic.st_mode & S_IXOTH)
-            putchar('x');
-        else
-            putchar('-');
-
-        user_id_statistic = getpwuid(object_statistic.st_uid);                   // returning pointer to pwd structrue 
-        printf("\t%s", user_id_statistic->pw_name);
+        printf( (object_stat.st_mode & S_IRUSR) ? "r" : "-");                 
+        printf( (object_stat.st_mode & S_IWUSR) ? "w" : "-");
+        printf( (object_stat.st_mode & S_IXUSR) ? "x" : "-");
         
-        group_id_statistic = getgrgid(object_statistic.st_gid);                 // return pointer to a structure with group's name
-        printf("\t%s", group_id_statistic->gr_name);
+        printf( (object_stat.st_mode & S_IRGRP) ? "r" : "-");
+        printf( (object_stat.st_mode & S_IWGRP) ? "w" : "-");                             // showing modes
+        printf( (object_stat.st_mode & S_IXGRP) ? "x" : "-");
         
-        printf("\t%ld", object_statistic.st_size);                              // show user information about current file
+        printf( (object_stat.st_mode & S_IROTH) ? "r" : "-");
+        printf( (object_stat.st_mode & S_IWOTH) ? "w" : "-");
+        printf( (object_stat.st_mode & S_IXOTH) ? "x" : "-"); 
+
+        
+        printf("\t%s", pass_stat->pw_name);
+        printf("\t%s", group_stat->gr_name);
+
+        printf("\t%ld", object_stat.st_size);
+
         printf("\t%s", string_pointer);
-        
+
         printf("\t%s", argv[1]);
-
-        putchar('\n'); 
-        
-
-
-    }
-    else if( (object_statistic.st_mode & S_IFMT) == S_IFDIR)
-    {
-        DIR * directory_pointer = opendir( argv[1] );                               // creating directory pointer and opening user's directory
-    
-        if(directory_pointer == NULL)                                               // NULL returned on error, show number of error in this situation
-        {
-            perror("Can't open this directory. Reason == >  ");
-            return 1;
-        }
  
-        struct dirent *directory_statistic;                                         // structure, from which i would take name of every file
-        struct passwd *user_id_statistic;                                           // structure, which contains data about passwords and important user_name
-        struct group  *group_id_statistic;                                          // structure, which contains data about groups
-
-
-        char * string_pointer;                                                      // pointer to string, which ctime function would return
-    
-        int files_count = 0;                                                        // total == > files count 
-        size_t helpful_size;                                                        // i need to replace new line character by null character. 
-    
-  
-        while( (directory_statistic = readdir(directory_pointer))  != NULL  )       // in the end of file or on error NULL returned
-        {
-            files_count++;               
-            stat(directory_statistic->d_name, &object_statistic);                  
-            // take name of current file, fill this structure with information about this particular file
-
-            string_pointer = ctime( &object_statistic.st_mtime);                    
-            helpful_size = strlen(string_pointer);                                  // replacing new line character by null character
-            string_pointer[helpful_size - 1] = '\0';                                // last one is '\0'. penultimate - \n. \n == > \0
-        
-            switch (object_statistic.st_mode & S_IFMT)                              // asking st_mode
-            {
-                case S_IFREG:                                                       // is it a regular file? put '-'
-                    putchar('-');
-                    break;
-                case S_IFDIR:                                                       // is it a directory? put 'd'
-                    putchar('d');
-                    break;
-                case S_IFBLK:                                                       // is it a block device? put 'b'
-                    putchar('b');
-                    break;
-                case S_IFCHR:                                                       // is it a character device? put 'c'
-                    putchar('c');
-                    break;
-                case S_IFLNK:                                                       // is it a symbolic link? put 'l'
-                    putchar('l');
-                    break;
-                case S_IFSOCK:                                                      // is it a socket? put 's'
-                    putchar('s');
-                    break;
-                default:                                                            // if an error occured
-                    putchar('?');
-                    break;                     
-            }
-        
-        
-            if(object_statistic.st_mode & S_IRUSR)
-                putchar('r');
-            else
-                putchar('-');
-           
-            if(object_statistic.st_mode & S_IWUSR)
-                putchar('w');
-            else
-                putchar('-');
-
-            if(object_statistic.st_mode & S_IXUSR)
-                putchar('x');
-            else
-                putchar('-');
-
-            if(object_statistic.st_mode & S_IRGRP)
-                putchar('r');
-            else
-                putchar('-');
-
-            if(object_statistic.st_mode & S_IWGRP)
-                putchar('w');
-            else
-                putchar('-');                                                       // asking st_mode: Can this read? Or write? Or execute?
-   
-            if(object_statistic.st_mode & S_IXGRP)
-                putchar('x');
-            else
-                putchar('-');
-
-            if(object_statistic.st_mode & S_IROTH)
-                putchar('r');
-            else
-                putchar('-');    
-
-            if(object_statistic.st_mode & S_IWOTH)
-                putchar('w');
-            else
-                putchar('-');     
-
-            if(object_statistic.st_mode & S_IXOTH)
-                putchar('x');
-            else
-                putchar('-');     
-        
-        
-            user_id_statistic = getpwuid(object_statistic.st_uid);                  // return pointer to a structure with user's name
-        
-            printf("\t%s", user_id_statistic->pw_name);
-
-            group_id_statistic = getgrgid(object_statistic.st_gid);                 // return pointer to a structure with group's name
-
-            printf("\t%s", group_id_statistic->gr_name);
-    
-            printf("\t%ld", object_statistic.st_size);                              // show user information about current file
-    
-            printf("\t%s", string_pointer);
-
-            printf("\t%s", directory_statistic->d_name);
-        
-            putchar('\n');                                                          // start printing information about new file with new line character
-        } 
-    
-        printf("total - %d", files_count);                                     
         putchar('\n');
 
-        if( (closedir(directory_pointer)) != 0)                                     // closing directory and looking for errors
+    }
+    
+    else if( (object_stat.st_mode & S_IFMT) == S_IFDIR)                                   // working with directory
+    {
+        DIR * dir_pointer = opendir( argv[1] ); 
+        if(dir_pointer == NULL)                                                           // problem with opening of directory
         {
-            perror("Can't close this directory. Reason == >  ");
+            perror("ls: can't open this directory: ");
+            return 1;
+        } 
+
+        struct dirent * dir_stat;
+        struct passwd * pass_stat;
+        struct group * group_stat;
+
+        char * string_pointer;
+
+        int files_count = 0;                                                              // my version of total -
+
+        while( (dir_stat = readdir(dir_pointer))  != NULL  )                              // opening directory
+        {
+            files_count++;
+
+            stat(dir_stat->d_name, &object_stat);
+            pass_stat = getpwuid(object_stat.st_uid);                                     // pulling information for structures
+            group_stat = getgrgid(object_stat.st_gid);  
+            
+            switch (object_stat.st_mode & S_IFMT)                                         // looking for types of files
+            {
+                case S_IFREG:                                                       
+                putchar('-');
+                break;
+                
+                case S_IFDIR:                                                       
+                putchar('d');
+                break;
+                
+                case S_IFBLK:                                                       
+                putchar('b');
+                break;
+                
+                case S_IFCHR:                                                       
+                putchar('c');
+                break;
+                
+                case S_IFLNK:                                                       
+                putchar('l');
+                break;
+                
+                case S_IFSOCK:                                                      
+                putchar('s');
+                break;
+                
+                default:                                                            
+                putchar('?');
+                break;                     
+            }
+            
+            
+            printf( (object_stat.st_mode & S_IRUSR) ? "r" : "-");
+            printf( (object_stat.st_mode & S_IWUSR) ? "w" : "-");
+            printf( (object_stat.st_mode & S_IXUSR) ? "x" : "-");
+        
+            printf( (object_stat.st_mode & S_IRGRP) ? "r" : "-");
+            printf( (object_stat.st_mode & S_IWGRP) ? "w" : "-");                         // showing modes
+            printf( (object_stat.st_mode & S_IXGRP) ? "x" : "-");
+        
+            printf( (object_stat.st_mode & S_IROTH) ? "r" : "-");
+            printf( (object_stat.st_mode & S_IWOTH) ? "w" : "-");
+            printf( (object_stat.st_mode & S_IXOTH) ? "x" : "-"); 
+
+            
+            string_pointer = ctime(&object_stat.st_mtime);
+            string_pointer[strlen(string_pointer) - 1] = '\0';                            // executing new line character from returned string
+
+            printf("\t%s", pass_stat->pw_name); 
+            printf("\t%s", group_stat->gr_name);
+
+            printf("\t%ld", object_stat.st_size);
+
+            printf("\t%s", string_pointer);
+
+            printf("\t%s", dir_stat->d_name);
+
+            putchar('\n');
+
+        }
+        
+        printf("total - %d", files_count);
+        putchar('\n');
+
+        if( closedir(dir_pointer) != 0)                                                   
+        {
+            perror("ls: can't close this directory: ");
             return 1;
         }
     }
-    return 0;
+    return 0;                                                                             // positive case
 }
