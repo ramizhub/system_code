0fe6f5c43b008cdf212652c684e848034c53a6d0
rhub <rearericu@gmail.com>
2022-02-17 00:52:18 +0400

    error handlings and short description of program work

--- psuax.c
+++ psuax.c
@@ -1,4 +1,8 @@
-// USER         PID %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND
+/*
+    It is a program inspired by the ps command that works with a, u, x keys.
+    All information is taken from the /proc folder. In particular, i parsed /proc/[PID]/stat , /proc/[PID]/statm files  and /proc/meminfo files.
+    Some of original ps aux components presented in a different form.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -36,8 +40,7 @@ main(void)
     struct passwd * pass_struct;                    // password structure -> "USER"
     struct dirent * dir_struct;                     // readdir return value, use dir_struct.d_name for every object name in this directory
     struct stat lstat_struct;                       // check every directory object for file type
-    struct rusage usage_struct;                     // getrusage return value 
-
+    
     char path[PATH_MAX];                            // final result -> "/proc/[pid]/stat"
     char statmpath[PATH_MAX];                       // final result -> "/proc/[pid]/statm"
     char pstatechar, char2;                         // pstate ( I, R, S etc) and supporting character
@@ -63,18 +66,23 @@ main(void)
     
     while( getc(fp) != SPACE)
         continue;
-    fscanf(fp, "%lu", &phymemory_kb );
+    
+    if( fscanf(fp, "%lu", &phymemory_kb) != 1 )
+    {
+        perror("ps: can't read value of MemTotal in /proc/meminfo file: ");
+        exit(EXIT_FAILURE);
+    }
     
     if( fclose(fp) != 0 )
     {
-        perror("ps: error in /proc/meminfo file opening: ");
+        perror("ps: error in /proc/meminfo file closing: ");
         exit(EXIT_FAILURE);
     }
 
     
     
     
-    printf("%s  %s %%%s  %%%s\t%s\t%s\t%s\t%s\t\t%s\t%s\n", "USER", "PID", "CPU", "MEM", "VSZ", "RSS", "STAT", "START", "UTIME", "COMMAND");
+    printf("%s  %s  %s  %%%s\t%s\t%s\t%s\t%s\t\t%s\t%s\n", "USER", "PID", "CPU", "MEM", "VSZ", "RSS", "STAT", "START", "UTIME", "COMMAND");
 
 
 
@@ -90,14 +98,20 @@ main(void)
         strncpy( statmpath, "/proc/", strlen("/proc/") + 1 );
         strncat( statmpath, dir_struct->d_name, strlen(dir_struct->d_name) + 1 );
         
-        lstat( path, &lstat_struct );
+        if( lstat(path, &lstat_struct) != 0 )
+        {
+            printf("\nps: error in lstat function with %s in first argument: ", path );
+            perror(NULL);
+            exit(EXIT_FAILURE);
+        }
 
         if( (lstat_struct.st_mode & S_IFMT) == S_IFDIR )
         {
             dp_inner = opendir(path);
             if(dp_inner == NULL)
             {
-                perror("ps: error in directory opening: ");
+                printf("\nps: error in %s directory opening: ", path );
+                perror(NULL);
                 exit(EXIT_FAILURE);
             }
 
@@ -113,17 +127,28 @@ main(void)
                 continue;
 
             pass_struct = getpwuid( lstat_struct.st_uid );
-            getrusage( atoi(dir_struct->d_name), &usage_struct );
+            if( pass_struct == NULL )
+            {
+                printf("\nps: error in getwpuid function with %d in argument: ", lstat_struct.st_uid );
+                perror(NULL);
+                exit(EXIT_FAILURE);
+            } 
             
-
-
+            
 
 //                                                              LOOK FOR 14th ELEMENT OF "/proc/[pid]/stat" - utime                      
             for( int index = 0; index < 13; index++ )
                 while( getc(fp) != SPACE)   
                     continue;
 
-            fscanf(fp, "%lu", &pwtime_sec );
+            
+            if( fscanf(fp, "%lu", &pwtime_sec) != 1 ) 
+            {
+                printf("\nps: can't read utime in %s: ", path );
+                perror(NULL);
+                exit(EXIT_FAILURE);
+            }
+            
             pwtime_sec = pwtime_sec / sysconf(_SC_CLK_TCK);
             rewind(fp);
 
@@ -135,7 +160,13 @@ main(void)
                 while( getc(fp) != SPACE)
                     continue;
             
-            fscanf(fp, "%c", &pstatechar);
+            if( fscanf(fp, "%c", &pstatechar) != 1 )
+            {
+                printf("\nps: can't read pstate in %s: ", path);
+                perror(NULL);
+                exit(EXIT_FAILURE);
+            }
+            
             rewind(fp);
             
             
@@ -146,7 +177,12 @@ main(void)
                 while( getc(fp) != SPACE)
                     continue;
             
-            fscanf(fp, "%lu", &vsz_kb);
+            if( fscanf(fp, "%lu", &vsz_kb) != 1 )
+            {
+                printf("\nps: can't read vsz in %s: ", path);
+                perror(NULL);
+                exit(EXIT_FAILURE);
+            }
             vsz_kb = vsz_kb / 1024;
             rewind(fp);
 
@@ -163,14 +199,20 @@ main(void)
             
             while( getc(fp_inner) != SPACE )
                 continue;
-            fscanf(fp_inner, "%ld", &rss_kb );
-            
+            if( fscanf(fp_inner, "%ld", &rss_kb) != 1 )
+            {
+                printf("can't read rss in %s: ", statmpath );
+                perror(NULL);
+                exit(EXIT_FAILURE);
+            }
+
             rss_kb = rss_kb * sysconf(_SC_PAGESIZE);
             rss_kb = rss_kb / 1024;
             
             if( fclose(fp_inner) != 0 )
             {
-                perror("ps: error in /proc/pid/statm file closing: ");
+                printf("error in %s file closing: ", statmpath );
+                perror(NULL);
                 exit(EXIT_FAILURE);
             }
 
@@ -182,8 +224,14 @@ main(void)
                 while( getc(fp) != ' ')
                     continue;
 
-            fscanf(fp, "%llu", &starttime );
-            seconds = starttime / sysconf(_SC_CLK_TCK);
+            if( fscanf(fp, "%llu", &starttime) != 1 )
+            {
+                printf("can't read starttime in %s: ", path);
+                perror(NULL);
+                exit(EXIT_FAILURE);
+            }
+            starttime = starttime / sysconf(_SC_CLK_TCK);
+            seconds = starttime;
             
             hours = seconds / 3600;
             seconds = seconds % 3600;
@@ -197,7 +245,7 @@ main(void)
 
 //                                                              PRINT INFO  
             
-            printf("%s  %s    %ld    %.2f  ", pass_struct->pw_name, dir_struct->d_name, usage_struct.ru_utime.tv_sec, (float)(rss_kb * 4) / phymemory_kb);
+            printf("%s  %s    %lld    %.2f  ", pass_struct->pw_name, dir_struct->d_name, starttime, (float)(rss_kb * 4) / phymemory_kb);
             printf(" %lu\t%lu\t %c\t", vsz_kb, rss_kb, pstatechar);
             printf("%u:%u:%u\t\t%lu\t", hours, minutes, seconds, pwtime_sec / 60);
 
@@ -217,11 +265,13 @@ main(void)
 //                                                              CLOSE "/proc/[pid]/stat" FILE AND "/[pid]" DIRECTORY 
             if( fclose(fp) != 0 )
             {
-                perror("ps: error in file closing: ");
+                printf("ps: error in %s file closing: ", path);
+                perror(NULL);
                 exit(EXIT_FAILURE);
-            } if(closedir(dp_inner) != 0 )
+            } if( closedir(dp_inner) != 0 )
             {
-                perror("ps: error in directory closing: ");
+                printf("ps: error in /proc/%s directory closing: ", dir_struct->d_name );
+                perror(NULL);
                 exit(EXIT_FAILURE);
             }
         }
@@ -230,7 +280,7 @@ main(void)
 //                                                              CLOSE "/proc" DIRECTORY 
     if( closedir(dp_proc) != 0 )
     {
-        perror("ps: error in proc directory closing: ");
+        perror("ps: error in /proc directory closing: ");
         exit(EXIT_FAILURE);
     }
 }
